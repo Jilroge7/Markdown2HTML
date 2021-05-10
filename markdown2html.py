20a8e562faa04387a9eeff062be441524d9c70f9
Jilroge7 <1672@holbertonschool.com>
2021-05-10 04:33:45 +0000

    adding partial advanced task 0

---
+++ markdown2html.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Script to take 2 args
+"""
+import sys
+import os
+from os import path
+from markdown import markdown
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+        exit(1)
+
+    elif len(sys.argv) == 3:
+        if not path.isfile(sys.argv[1]):
+            sys.stderr.write("Missing {}\n".format(sys.argv[1]))
+            exit(1)
+
+    else:
+        with open(sys.argv[1]) as mfile:
+            with open(sys.argv[2], 'w') as html:
+                exit(0)
